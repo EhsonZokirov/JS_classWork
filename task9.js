44338cee28deaf99c1b6554cb3095de826334bc0
Ehson Zokirov <zokirov_2000@mail.ru>
2023-02-15 13:34:42 +0500

    comited

--- task9.js
+++ task9.js
@@ -5,3 +5,13 @@ Write a program to print the factorial of given number
 
 Программаи чопи факториали адади додашударо нависед
 */
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(5));
