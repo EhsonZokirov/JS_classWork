7613bbc4b80cd09b8873c437574ef9ba0d58347d
Ehson Zokirov <zokirov_2000@mail.ru>
2023-02-15 14:10:59 +0500

    isPalindrome

--- task10.js
+++ task10.js
@@ -6,3 +6,12 @@ Determining if a number is a palindrome
 Муайян кардани он, ки рақам палиндром аст
 */
 
+function isPalindrome(numString = "") {
+  let res = "";
+  for (let i = numString.length - 1; i >= 0; i--) {
+    res += numString[i];
+  }
+  return res == numString;
+}
+
+console.log(isPalindrome("101"));
