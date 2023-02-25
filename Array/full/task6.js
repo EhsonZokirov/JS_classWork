7cb5da7351aeede839ef405c9dc70292b83c1f93
Ehson Zokirov <zokirov_2000@mail.ru>
2023-02-25 15:16:20 +0500

    reverseARR

--- Array/full/task6.js
+++ Array/full/task6.js
@@ -1,4 +1,11 @@
 function reverseArr(arr) {
-  return arr.toString().split("").reverse();
+  let res = [];
+  let arr2 = arr.toString().split("").reverse();
+  arr2 = arr2.filter((e)=>{
+    if(e == "1" ||  e == "2" ||e == "3" ||e == "4" ||e == "5" ||e == "6" ||e == "7" ||e == "8" ||e == "9"){
+      res.push(Number(e));
+    }
+  })
+  return res
 }
 console.log(reverseArr(12345));
