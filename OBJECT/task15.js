20ac846a4f48dfd044871398f1ed5dffb2ffe6bc
Ehson Zokirov <zokirov_2000@mail.ru>
2023-03-01 21:36:48 +0500

    add task

---
+++ OBJECT/task15.js
@@ -0,0 +1,6 @@
+function volumeOfBox(obj) {
+  return obj.length * obj.width * obj.height;
+}
+console.log(volumeOfBox({ width: 2, length: 5, height: 1 })); // 10
+console.log(volumeOfBox({ width: 4, length: 2, height: 2 })); // 16
+console.log(volumeOfBox({ width: 2, length: 3, height: 5 })); // 30
