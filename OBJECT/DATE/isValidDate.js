5d904292cf6661a4e3a808be85ce0fd78f81789f
Ehson Zokirov <zokirov_2000@mail.ru>
2023-03-01 17:07:50 +0500

    asd

--- OBJECT/DATE/isValidDate.js
+++ OBJECT/DATE/isValidDate.js
@@ -2,9 +2,9 @@
 Учитывая параметры день, месяц и год, верните, является ли эта дата допустимой датой.
 */
 
-function isValidDate(date) {
-  let date2 = new Date(date);
-  return;
+function isValidDate(date, month, year) {
+  let dates = new Date(year, month - 1, date);
+  return ( dates.getFullYear() == year && dates.getMonth() + 1 == month && dates.getDate() == date)
 }
 console.log(isValidDate(35, 2, 2020)); // false
 // February doesn't have 35 days.
@@ -12,3 +12,5 @@ console.log(isValidDate(8, 3, 2020)); // true
 // 8th March 2020 is a real date.
 console.log(isValidDate(31, 6, 1980)); // false
 // June only have 30 daysю
+
+
